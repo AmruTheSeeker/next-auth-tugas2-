f5f454d5f30679f8283a246dd63677de3c36a000
Amru <second.kautsar@gmail.com>
2023-11-30 08:19:01 +0700

    feature: using oauth, usesession, serverserssion, extended session, and extended jwt

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,29 @@
-import { Counter } from '@/components/counter';
+'use client';
+
+import { signIn, signOut, useSession } from 'next-auth/react';
+
+import { Login } from '@/components/Auth/Login';
+import { Button } from '@/components/ui/button';
 
 export default function Page() {
-  return <Counter />;
+  const { data: session } = useSession();
+
+  console.log(session);
+
+  return (
+    <div>
+      {session ? (
+        <div className="flex justify-between p-8">
+          <div className="text-lg font-bold">App.</div>
+          <div className="flex item-center gap-4">
+            <div>{session.user?.name}</div>
+            <img alt="" src={session.user?.image as string} width={40} height={40} className="rounded-full" />
+            <Button onClick={() => signOut()}>Logout</Button>
+          </div>
+        </div>
+      ) : (
+        <Login />
+      )}
+    </div>
+  );
 }
---
+++ src/middleware.ts
@@ -0,0 +1,22 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { decode } from 'next-auth/jwt';
+
+export default async function middleware(req: NextRequest) {
+  const token = req.cookies.get('next-auth.session-token')?.value;
+
+  const payload = await decode({
+    token,
+    secret: process.env.NEXTAUTH_SECRET as string,
+  });
+  console.log(payload?.role);  
+
+  if (!token) {
+    return NextResponse.redirect(new URL('/', req.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ['/admin', '/dashboard'],
+};
